ece94113bd30ea214135daa54fcd5f7429617876
judie97 <127154363+judie97@users.noreply.github.com>
2023-03-06 23:36:27 +0100

    Update Exact Postage.py

--- Exact Postage.py
+++ Exact Postage.py
@@ -1,24 +1,12 @@
 def postalValidate(S):
-    X = chr(32)
-    S = S.replace(X, '')
-    if len(S) != 6:
-        return False
-    for i in S:
-        for n in S:
-            if S.isalpha() or S.isdigit():
-                return False
-            elif S[0].isdigit():
-                return False
-            elif S[2].isdigit():
-                return False
-            elif S[4].isdigit():
-                return False
-            elif S[1].isalpha():
-                return False
-            elif S[3].isalpha():
-                return False
-            elif S[5].isdigit() == False:
-                return False            
-            else:
-                 break
-        return S.upper()
+    S = S.replace(" ", "")
+    if len(S) == 6:
+       digit = S[::-2]
+       alpha = S[::2]
+       if alpha.isalpha() and digit.isdigit():
+           return S.upper()            
+       else:
+           return False
+    else:
+       return False
+       
